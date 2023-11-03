9ef94ca56537c312d9924c8710d7cb9e5a1b6cdf
Alberto Blacutt <albertoblacutt@Albertos-MacBook-Pro.local>
2023-11-03 09:26:11 -0400

    DE-554 - DE 597 Added eslint check and prettier format

--- src/e2e/index.spec.ts
+++ src/e2e/index.spec.ts
@@ -1,8 +1,8 @@
-import { createClient } from '../config'
+import { createClient } from '../config';
 
-describe("client initialization", () => {
-  test("should generate a valid client instance when config is executed as singleton", () => {
-    const client = createClient()
-    expect(client).not.toBeNull()
+describe('client initialization', () => {
+  test('should generate a valid client instance when config is executed as singleton', () => {
+    const client = createClient();
+    expect(client).not.toBeNull();
   });
 });
--- src/e2e/utils/index.ts
+++ src/e2e/utils/index.ts
@@ -1,20 +1,20 @@
-import { createClient } from '../../config'
+import { createClient } from '../../config';
 import { SitesController } from '../../controllers/sitesController';
-
 
 const DELAY_AFTER_CLEAN = 1000;
 // Based on https://developers.chargify.com/docs/api-docs/c912e634019c9-clear-site-data
 // doc recommendation added small delay a site verification or read retry command
 export async function cleanSite() {
-    return new Promise((resolve, reject) => {
-        const client = createClient();
-        const sitesController = new SitesController(client);
-        sitesController.clearSite()
-            .then(response => {
-                setTimeout(() => {
-                    resolve(response)
-                }, DELAY_AFTER_CLEAN);
-            })
-            .catch(error => reject(error))
-    });
+  return new Promise((resolve, reject) => {
+    const client = createClient();
+    const sitesController = new SitesController(client);
+    sitesController
+      .clearSite()
+      .then((response) => {
+        setTimeout(() => {
+          resolve(response);
+        }, DELAY_AFTER_CLEAN);
+      })
+      .catch((error) => reject(error));
+  });
 }
