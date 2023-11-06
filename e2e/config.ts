f983f1d724c43be21eba460fdf3ad78a47243b01
Alberto Blacutt <albertoblacutt@Albertos-MacBook-Pro.local>
2023-11-06 14:47:34 -0400

    revert SDK-hot-merge integration from publish branch

---
+++ e2e/config.ts
@@ -0,0 +1,22 @@
+import { Client, Environment } from '../src';
+
+export const CONFIG = {
+  SDK_KEY: process.env.SDK_KEY,
+  PASSWORD: process.env.PASSWORD,
+  SUBDOMAIN: process.env.SUBDOMAIN,
+  DOMAIN: process.env.DOMAIN,
+};
+
+const defaultOptions = {
+  timeout: 0,
+  domain: CONFIG.DOMAIN,
+  environment: Environment.Production,
+  subdomain: CONFIG.SUBDOMAIN,
+  basicAuthUserName: CONFIG.SDK_KEY,
+  basicAuthPassword: CONFIG.PASSWORD,
+};
+
+export function createClient(options = defaultOptions) {
+  const client = new Client(options);
+  return client;
+}
