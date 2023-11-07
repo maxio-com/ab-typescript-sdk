83031a24a7c6ca2a3b6c3e08280cb3f67cf8bd9c
maxio-sdk <maxio-sdk@maxio.com>
2023-11-07 12:55:50 +0000

    Automated commit message

--- src/models/subscriptionGroupInlined.ts
+++ src/models/subscriptionGroupInlined.ts
@@ -4,7 +4,7 @@
  * This file was automatically generated for Maxio by APIMATIC v3.0 ( https://www.apimatic.io ).
  */
 
-import { object, optional, Schema, string } from '../schema';
+import { boolean, object, optional, Schema, string } from '../schema';
 
 export interface SubscriptionGroupInlined {
   /** The UID for the group */
@@ -14,7 +14,7 @@ export interface SubscriptionGroupInlined {
   /** The subscription ID of the primary within the group. Applicable to scheme 1. */
   primarySubscriptionId?: string;
   /** A boolean indicating whether the subscription is the primary in the group. Applicable to scheme 1. */
-  primary?: string;
+  primary?: boolean;
 }
 
 export const subscriptionGroupInlinedSchema: Schema<SubscriptionGroupInlined> = object(
@@ -22,6 +22,6 @@ export const subscriptionGroupInlinedSchema: Schema<SubscriptionGroupInlined> = 
     uid: ['uid', optional(string())],
     scheme: ['scheme', optional(string())],
     primarySubscriptionId: ['primary_subscription_id', optional(string())],
-    primary: ['primary', optional(string())],
+    primary: ['primary', optional(boolean())],
   }
 );
