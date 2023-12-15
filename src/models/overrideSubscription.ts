c1f5e28bd660b773f44f391947a395f8c7c33c63
Michal <michal.pierog@chargify.com>
2023-12-15 14:29:19 +0100

    0.0.3 Release

--- src/models/overrideSubscription.ts
+++ src/models/overrideSubscription.ts
@@ -7,15 +7,15 @@
 import { object, optional, Schema, string } from '../schema';
 
 export interface OverrideSubscription {
-  /** Can be used to record an external signup date. Chargify uses this field to record when a subscription first goes active (either at signup or at trial end) */
+  /** Can be used to record an external signup date. Chargify uses this field to record when a subscription first goes active (either at signup or at trial end). Only ISO8601 format is supported. */
   activatedAt?: string;
-  /** Can be used to record an external cancellation date. Chargify sets this field automatically when a subscription is canceled, whether by request or via dunning. */
+  /** Can be used to record an external cancellation date. Chargify sets this field automatically when a subscription is canceled, whether by request or via dunning. Only ISO8601 format is supported. */
   canceledAt?: string;
   /** Can be used to record a reason for the original cancellation. */
   cancellationMessage?: string;
-  /** Can be used to record an external expiration date. Chargify sets this field automatically when a subscription expires (ceases billing) after a prescribed amount of time. */
+  /** Can be used to record an external expiration date. Chargify sets this field automatically when a subscription expires (ceases billing) after a prescribed amount of time. Only ISO8601 format is supported. */
   expiresAt?: string;
-  /** Can only be used when a subscription is unbilled, which happens when a future initial billing date is passed at subscription creation. The value passed must be before the current date and time. Allows you to set when the period started so mid period component allocations have the correct proration. */
+  /** Can only be used when a subscription is unbilled, which happens when a future initial billing date is passed at subscription creation. The value passed must be before the current date and time. Allows you to set when the period started so mid period component allocations have the correct proration. Only ISO8601 format is supported. */
   currentPeriodStartsAt?: string;
 }
 
